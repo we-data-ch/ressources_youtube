4a903a375c69d20e197f203ee4f42820ed662fce
Vestin Hategekimana <80328639+CyuHat@users.noreply.github.com>
2025-02-27 23:59:00 +0100

    Update tuto_ollama
    Update in Julia and JavaScript files

--- Ollama/Introduction/tuto_ollama.jl
+++ Ollama/Introduction/tuto_ollama.jl
@@ -6,9 +6,11 @@ schema = PT.OllamaSchema()
 # Chat
 res = aigenerate(schema, "Quel langage de programmation fonctionnel choisir?"; model="qwen2.5:3b")
 
+println(res)
+
 # Embedding
 texte = ["C'est un texte.", "Une phrase courte."]
 
 embedding = aiembed(schema, texte, copy; model="qwen2.5:3b")
 
-embedding.content
+println(embedding.content)
--- Ollama/Introduction/tuto_ollama.mjs
+++ Ollama/Introduction/tuto_ollama.mjs
@@ -2,9 +2,9 @@
 import ollama from 'ollama'
 
 // Fonctions de base
-// console.log(await ollama.list())
-// console.log(await ollama.show({model: 'qwen2.5:3b'}))
-// console.log(await ollama.pull({model: 'nomic-embed-text'}))
+console.log(await ollama.list())
+console.log(await ollama.show({model: 'qwen2.5:3b'}))
+console.log(await ollama.pull({model: 'nomic-embed-text'}))
 
 // Prompt simple
 let res1 = await ollama.generate({
@@ -20,4 +20,4 @@ let response = await ollama.chat({
     messages: [{ role: 'user', content: 'Qui es-tu?' }],
   })
 
-// console.log(response.message.content)
+console.log(response.message.content)
